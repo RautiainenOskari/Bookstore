b86c5d3abcc904d97f780f7a978c7861f0633a4e
Oskari Rautiainen <rautiainen.oskarir@gmail.com>
2023-09-08 01:44:55 +0300

    First commit

---
+++ src/main/java/project/Bookstore/domain/Book.java
@@ -0,0 +1,39 @@
+package project.Bookstore.domain;
+
+public class Book {
+	private String title;
+	private String author;
+	private Integer publicationYear;
+	private Integer isbn;
+	private Double price;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Integer getPublicationYear() {
+		return publicationYear;
+	}
+	public void setPublicationYear(Integer publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+	public Integer getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(Integer isbn) {
+		this.isbn = isbn;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
