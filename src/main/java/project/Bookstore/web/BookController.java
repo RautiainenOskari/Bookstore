1dd689d7c5e372d2d28a99733367b224806baa75
Oskari Rautiainen <rautiainen.oskarir@gmail.com>
2023-09-13 21:05:07 +0300

    Book hmtl interface, add and delete

--- src/main/java/project/Bookstore/web/BookController.java
+++ src/main/java/project/Bookstore/web/BookController.java
@@ -1,16 +1,43 @@
 package project.Bookstore.web;
 
+import project.Bookstore.domain.Book;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import project.Bookstore.domain.BookRepository;
 @Controller
-@ResponseBody
 public class BookController {
+		@Autowired
+		private BookRepository repository;
+		 
+		@RequestMapping("/booklist")
+		public String bookList(Model model) {
+			model.addAttribute("Books", repository.findAll());
+			return "booklist";
+		}
+		
+		@RequestMapping(value = "/add")
+		public String addBook (Model model) {
+			model.addAttribute("book", new Book());
+			return "addBook";
+		}
 
-		@RequestMapping("/index")
-		public String hello() {
-			return "Index page";
+		
+		@RequestMapping(value = "/save", method = RequestMethod.POST)
+		public String saveBook (Book book) {
+			repository.save(book);
+			return "redirect:booklist";
+		}
+		
+		@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+		public String deleteBook (@PathVariable("id") Long bookId, Model model) {
+			repository.deleteById(bookId);
+			return "redirect:../booklist";
 		}
 
 	}
