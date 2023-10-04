cc149d5347930745cd8464ffc964f70871b73259
Oskari Rautiainen <rautiainen.oskarir@gmail.com>
2023-10-04 21:49:20 +0300

    Added AppUser/Repository,Userdetailservice, userdata for commandlinerunner

--- src/main/java/project/Bookstore/BookstoreApplication.java
+++ src/main/java/project/Bookstore/BookstoreApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-
+import project.Bookstore.domain.AppUser;
+import project.Bookstore.domain.AppUserRepository;
 import project.Bookstore.domain.Book;
 import project.Bookstore.domain.BookRepository;
 import project.Bookstore.domain.Category;
@@ -19,7 +20,7 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository, AppUserRepository urepository) {
 		return (args) -> {
 			crepository.save(new Category("Tietokirja"));
 			crepository.save(new Category("Fantasia"));
@@ -28,6 +29,10 @@ public class BookstoreApplication {
 			brepository.save(new Book("Kalakirja", "Joku Jokunen",crepository.findByName("Tietokirja").get(0), 0002, 2014, 9.99));
 			brepository.save(new Book("Juustot", "Paavo Mäkinen", crepository.findByName("Tietokirja").get(0), 0003, 2022, 5.99));
 			
+			AppUser user1 = new AppUser("user", "$2a$10$o6twiNXVguXbnXPrzHqyOuLJE4HN8pmif19zHTovqedo0WvoHHS0.", "USER");
+			AppUser user2 = new AppUser("admin", "$2a$10$8mStTRGNkNHx3CnWIUOycecwz.STUKcscmC0..FvRTi.Y9PWPKfEu", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 
 	};
 	
--- src/main/java/project/Bookstore/WebSecurityConfig.java
+++ src/main/java/project/Bookstore/WebSecurityConfig.java
@@ -3,19 +3,28 @@ package project.Bookstore;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration @EnableWebSecurity
-public class WebSecurityConfig
-{
+import project.Bookstore.web.UserDetailServiceImpl;
+
+@Configuration
+@EnableMethodSecurity(securedEnabled = true)
+@EnableWebSecurity
+public class WebSecurityConfig {
+	@Autowired
+	private UserDetailServiceImpl userDetailsService;
 @Bean
 public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 http
@@ -30,24 +39,9 @@ http
 return http.build();
 }
 
-@Bean
-public UserDetailsService userDetailsService() {
-UserDetails user = User.withDefaultPasswordEncoder()
-.username("user")
-.password("password")
-.roles("USER")
-.build();
-List<UserDetails> users = new ArrayList();
-users.add(user);
-
-user = User.withDefaultPasswordEncoder()
-.username("admin")
-.password("admin")
-.roles("USER", "ADMIN")
-.build();
-
-users.add(user);
-return new InMemoryUserDetailsManager(users);
+@Autowired
+public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 }
 
 }
